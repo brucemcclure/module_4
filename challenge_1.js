c28145696dae6839a35f032ac890230f1eb8c503
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-15 16:20:13 +1000

    Initial commit

---
+++ challenge_1.js
@@ -0,0 +1,19 @@
+// let user = "Tom";
+// 
+// if (user === 'Tom') {
+//     console.log("Hello Tom");
+//   } else {
+//     console.log("Hello someone other than Tom");
+//   }
+// 
+// let age = prompt("How old are you?");
+// 
+//   if (age >= 18) {
+//     console.log("welcome to the casino!");
+//   } else {
+//     alert("Alert!! The salamanders have escaped!!!!");
+//   }
+
+
+let students = [];
+students.push("Sarah");
