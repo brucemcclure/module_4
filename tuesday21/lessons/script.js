da2c25225f6fdc22c28ab722b4e6e3c761da3fea
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-21 10:38:12 +1000

    Classes

--- tuesday21/lessons/script.js
+++ tuesday21/lessons/script.js
@@ -1,1 +1,14 @@
-console.log("Mr pickles the corgi")
+console.log("Mr pickles the corgi");
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    area() {
+       return this.height * this.width;
+    }
+}
+
+let rect = new Rectangle(20, 10);
