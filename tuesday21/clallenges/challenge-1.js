8ad0b610ccdc595dd2553ce2695a83bd2d832d2e
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-21 10:13:38 +1000

    Tuesday 1st commit

---
+++ tuesday21/clallenges/challenge-1.js
@@ -0,0 +1,8 @@
+//Use ES6 whenever possible.
+
+/*
+CHALLENGE 1
+Create a function sum() that will sum all arguments passed to it.
+Quantity of the arguments is unknown.
+Use console.log() at the end of the sum() function to print result.
+*/
---
+++ tuesday21/lessons/script.js
@@ -0,0 +1,1 @@
+console.log("Mr pickles the corgi")
