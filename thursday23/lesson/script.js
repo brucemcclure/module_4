79d3e1b13f61b2bffa2fcff7df207d34202571cf
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-23 10:38:30 +1000

    call backs

--- thursday23/lesson/script.js
+++ thursday23/lesson/script.js
@@ -1,1 +1,14 @@
-console.log("Mr Pickles the corgi")
+document.write("Mr Pickles the corgi")
+
+function adder(x,y, callback) {
+    let answer = x + y;
+
+    if (callback){
+        return callback(answer);
+    }
+    return answer;
+}
+
+    function shoutSomething(input) {
+        console.log(`${input}!!!!!`);
+    }
