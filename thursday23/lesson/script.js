39782df1a9dc0233be27ca7861a0863cfd443abf
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-23 12:02:32 +1000

    using web apis for asyncronous javascript

--- thursday23/lesson/script.js
+++ thursday23/lesson/script.js
@@ -1,19 +1,30 @@
 document.write("Mr Pickles the corgi")
 
+// console.log(1);
+// setTimeout(() => {
+//     console.log(2);
+// }, 0);
+
+// console.log(3);
+
+function wait(ms) {
+    let start = Date.now();
+    let now = start;
+
+    while(now - start < ms) {
+        now = Date.now();
+    }
+}
+
 document.getElementById("button").addEventListener("click", () => {
+    setTimeout(() => {
     wait(5000);
     alert("Ran!");
-});
+}, 0);
+})
 
 
-// function wait(ms) {
-    // let start = Date.now();
-    // let now = start;
-// 
-    // while(now - start < ms) {
-        // now = Date.now();
-    // }
-// }
-// 
-// wait(3000);
-// console.log("Finished");
+wait(3000);
+console.log("Finished");
+
+
