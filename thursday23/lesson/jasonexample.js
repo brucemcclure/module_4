80484ad8e54c14a53e9e3b9a7cc7bec178db84f8
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-23 10:29:47 +1000

    XML and JSON

---
+++ thursday23/lesson/jasonexample.js
@@ -0,0 +1,5 @@
+let stuff = "[{\"name\":\"K.West\",\"nickname\":\"Ye\",\"dateOfBirth\":\"1977-06-08\"}, {\"name\":\"A. Graham\",\"nickname\":\"Drake\",\"dateOfBirth\":\"1986-10-24\"}]"
+
+let cleanedUp = JSON.parse(stuff);
+
+let stringify = JSON.stringify(stuff);
